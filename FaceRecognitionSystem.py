d39bf10338f0fe0bcb29df8cf4b171c4675cfc62
Enrique <hsandovals@uni.pe>
2024-05-23 22:05:00 -0500

    commint

--- FaceRecognitionSystem.py
+++ FaceRecognitionSystem.py
@@ -415,7 +415,7 @@ def Sign_Biometric():
                 for rostros in res.multi_face_landmarks:
 
                     # Draw Face Mesh
-                    mpDraw.draw_landmarks(frame, rostros, FacemeshObject.FACE_CONNECTIONS, ConfigDraw, ConfigDraw)
+                    mpDraw.draw_landmarks(frame, rostros, FacemeshObject.FACEMESH_TESSELATION, ConfigDraw, ConfigDraw)
 
                     # Extract KeyPoints
                     for id, puntos in enumerate(rostros.landmark):
